cf793e3aadf3a4eec671b479b72f25170dc0addb
jenrose92 <jennyrose.orolfo@gmail.com>
2024-04-24 00:14:28 -0600

    in this commit I tried to create a partial scss -i love it the idea of the partial, make the code cleaner

---
+++ about/scripts.js
@@ -0,0 +1,13 @@
+// adding clasa to selected elements
+
+$('h4').addClass("poppins-light");
+
+// h5
+$('h5').addClass("poppins-light");
+
+
+// h6
+$('h6').addClass("poppins-regular");
+
+
+$('p').addClass("poppins-regular");
